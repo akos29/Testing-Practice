9f27d850043a0a74b12e98116558d612e36f48b7
unknown <kibrish49@gmail.com>
2022-11-15 20:29:49 +0200

    Add Task 1 to test the string length as per the requirment

---
+++ stringLength.test.js
@@ -0,0 +1,14 @@
+const stringLength = require('./stringLength');
+const sum = require('./stringLength');
+
+test('takes a string and returns its characters count', () => {
+  expect(stringLength("Kibrewosse")).toBe(10);
+});
+
+test('check character less than 1', () => {
+  expect(()=>stringLength("")).toThrow("The character length must between one and ten");
+});
+
+test('check character greater than 10', () => {
+  expect(()=>stringLength("Kibrewossen")).toThrow("The character length must between one and ten");
+});
